1bd11b90a308ed32e252bc59f7aafce6cb0e009f
Ervin Chow <ervinchow@gmail.com>
2019-07-11 20:38:27 -0700

    Initial Commit

---
+++ gigs/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  gigs
+//
+//  Created by Ervin Chow on 7/11/19.
+//  Copyright © 2019 Ervin Chow. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
